2dd85e38334ad66d34a2f1059cde411abfc51a5e
Naviththa BAthisa <naviththabathisa@gmail.com>
2025-08-17 00:42:27 +1200

    Implement subtract() to satisfy tests

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -1,14 +1,12 @@
 package org.example;
 
 public class Calc {
-
-    public int add(int x, int y)
-    {
-        return x+y;
+    public int add(int a, int b) {
+        return a + b;
     }
 
-    public int subtract(int x, int y)
-    {
-        return x*y;
+    // MUST be subtraction, not multiply or add
+    public int subtract(int a, int b) {
+        return a - b;
     }
 }
--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -1,18 +1,19 @@
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class CalcTest {
 
     @Test
-    public void testAddition() {
+    void testAddition() {
         Calc c = new Calc();
         assertEquals(5, c.add(2, 3));
     }
-    
+
     @Test
-    public void testSubtraction() {
+    void testSubtraction() {
         Calc c = new Calc();
         assertEquals(2, c.subtract(4, 2));
     }
 }
-
